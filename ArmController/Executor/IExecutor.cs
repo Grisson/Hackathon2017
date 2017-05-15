abc3d8302bdd720d143a2799a09e304974410806
Grisson Wang <grisson@gmail.com>
2017-05-14 18:04:42 -0700

    add executor interface

---
+++ ArmController/Executor/IExecutor.cs
@@ -0,0 +1,14 @@
+﻿using ArmController.lib.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmController.Executor
+{
+    interface IExecutor
+    {
+        void Execute(BaseCommand command);
+    }
+}
