ded2d588a18a71ba599fc87cf1ebf84e495871c0
Grisson Wang <grisson@gmail.com>
2017-05-28 15:29:13 -0700

    add arm web apis

--- ArmApi/Controllers/ArmController.cs
+++ ArmApi/Controllers/ArmController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArmController.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +8,50 @@ using System.Web.Http;
 
 namespace ArmApi.Controllers
 {
+    [RoutePrefix("api/arm")]
     [ServiceRequestActionFilter]
     public class ArmController : ApiController
     {
-        public IEnumerable<string> Get()
+        [Route("register")]
+        [HttpGet]
+        public IEnumerable<string> Register()
         {
             return new string[] { "value1", "value2" };
+        }
+
+        [Route("reporttouch/{timeStamp}/{x:double}/{y:double}")]
+        [HttpPut]
+        public string ReportTouch()
+        {
+            return "ReportTouch";
+        }
+
+        [Route("reportpose/{timeStamp}/{x:int}/{y:int}/{z:int}")]
+        [HttpPut]
+        public string ReportPose(string timeStamp, int x, int y, int z)
+        {
+            return "ReportPose";
+        }
+
+        [Route("startcalibrate")]
+        [HttpGet]
+        public string StartCalibrate()
+        {
+            return "Calibrate commands";
+        }
+
+        [Route("EndCalibrate")]
+        [HttpGet]
+        public string EndCalibrate()
+        {
+            return "Calibrated";
+        }
+
+        [Route("topose/{x:double}/{y:double}")]
+        [HttpGet]
+        public IHttpActionResult ConvertToPose()
+        {
+            return Ok<PosePosition>(new PosePosition(1,2,3));
         }
     }
 }
--- ArmApi/Startup.cs
+++ ArmApi/Startup.cs
@@ -12,6 +12,8 @@ namespace ArmApi
             // Configure Web API for self-host. 
             HttpConfiguration config = new HttpConfiguration();
 
+            config.MapHttpAttributeRoutes();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
