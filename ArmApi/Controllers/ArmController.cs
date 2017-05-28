f88729de26225529fb1425961e143ff84e02092f
Grisson Wang <grisson@gmail.com>
2017-05-28 14:35:10 -0700

    add arm controller

---
+++ ArmApi/Controllers/ArmController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ArmApi.Controllers
+{
+    [ServiceRequestActionFilter]
+    public class ArmController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
