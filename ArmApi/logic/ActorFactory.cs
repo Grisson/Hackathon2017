9a46414de8bf3179077da9a02ed03d821af685fa
Grisson Wang <grisson@gmail.com>
2017-05-28 17:32:06 -0700

    add missing class

---
+++ ArmApi/logic/ActorFactory.cs
@@ -0,0 +1,26 @@
+﻿using ArmActor.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+
+namespace ArmApi.logic
+{
+    public static class ActorFactory
+    {
+        private static readonly Uri ArmActorUrl = new Uri("fabric:/CloudBrian/ArmActorService");
+
+        public static IArmActor GetArm(string actorId)
+        {
+            return ActorProxy.Create<IArmActor>(new ActorId(actorId), ArmActorUrl);
+        }
+
+        public static IArmActor GetArm(ActorId actorId)
+        {
+            return ActorProxy.Create<IArmActor>(actorId, ArmActorUrl);
+        }
+    }
+}
