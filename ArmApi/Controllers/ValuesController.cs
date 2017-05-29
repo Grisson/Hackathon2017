160de55986d7f4b49038f5bffebb855601ed9f59
Grisson Wang <grisson@gmail.com>
2017-05-29 15:24:13 -0700

    remove unused actions in default controller

--- ArmApi/Controllers/ValuesController.cs
+++ ArmApi/Controllers/ValuesController.cs
@@ -11,26 +11,5 @@ namespace ArmApi.Controllers
         {
             return new string[] { "value1", "value2" };
         }
-
-        // GET api/values/5 
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values 
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5 
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5 
-        public void Delete(int id)
-        {
-        }
     }
 }
